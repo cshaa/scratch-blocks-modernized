0538b2624a096bb0e7724b79b356feda789dc75b
m93a <m93a.cz@gmail.com>
2022-03-18 14:40:15 +0100

    tried building with rollup, but it's a dead end

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        input: 'dist/horizontal.js',
+        output: {
+            format: 'es',
+            file: 'dist/es/horizontal.js',
+        }
+    },
+    {
+        input: 'dist/vertical.js',
+        output: {
+            format: 'es',
+            file: 'dist/es/vertical.js',
+        }
+    },
+];
